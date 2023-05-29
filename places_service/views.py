526cd8930ee13bfd5a7f79ba590de2c5a8356ad2
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2023-05-29 10:40:43 +0300

    add extend_schemas to all PlaceViewSet actions

--- places_service/views.py
+++ places_service/views.py
@@ -1,3 +1,6 @@
+from django.core.exceptions import ValidationError
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import extend_schema, OpenApiExample, OpenApiParameter
 from rest_framework import viewsets
 from rest_framework.pagination import PageNumberPagination
 
@@ -20,10 +23,101 @@ class PlaceViewSet(viewsets.ModelViewSet):
     serializer_class = PlaceSerializer
     pagination_class = PlacePagination
 
+    @extend_schema(
+        description="Retrieve a list of places.",
+        responses={200: PlaceSerializer(many=True)},
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @extend_schema(
+        description="Create a new place.",
+        request=PlaceSerializer,
+        responses={201: PlaceSerializer},
+        examples=[
+            OpenApiExample(
+                name="Example 1 Create Place",
+                value={
+                    "name": "New Place",
+                    "description": "A new place",
+                    "geom": "SRID=4326;POINT(15 15)"
+                },
+                response_only=False
+            )
+        ]
+    )
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    @extend_schema(
+        description="Retrieve a single place by ID.",
+        responses={200: PlaceSerializer()},
+    )
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+
+    @extend_schema(
+        description="Update a place by ID.",
+        request=PlaceSerializer,
+        responses={200: PlaceSerializer()},
+    )
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+
+    @extend_schema(
+        description="Partial update of a place by ID.",
+        request=PlaceSerializer,
+        responses={200: PlaceSerializer()},
+    )
+    def partial_update(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
+
+    @extend_schema(
+        description="Delete a place by ID.",
+        responses={204: None},
+    )
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="latitude",
+                description="Latitude coordinate of the target location.",
+                required=True,
+                type=OpenApiTypes.NUMBER,
+                location=OpenApiParameter.QUERY,
+            ),
+            OpenApiParameter(
+                name="longitude",
+                description="Longitude coordinate of the target location.",
+                required=True,
+                type=OpenApiTypes.NUMBER,
+                location=OpenApiParameter.QUERY,
+            ),
+        ],
+        description="Find the closest place to the given coordinates.",
+        responses={200: PlaceSerializer()},
+        examples=[
+            OpenApiExample(
+                name="Find Closest Place",
+                value={
+                    "latitude": 5,
+                    "longitude": 5
+                },
+            )
+        ],
+    )
     @action(detail=False, methods=["get"])
     def closest_point(self, request):
-        latitude = float(request.query_params.get("latitude"))
-        longitude = float(request.query_params.get("longitude"))
+        latitude_str = request.query_params.get("latitude")
+        longitude_str = request.query_params.get("longitude")
+
+        try:
+            latitude = float(latitude_str)
+            longitude = float(longitude_str)
+        except ValueError:
+            raise ValidationError("Invalid latitude or longitude values.")
 
         point = Point(longitude, latitude, srid=4326)
 
