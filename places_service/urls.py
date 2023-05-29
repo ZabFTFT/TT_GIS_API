b82ce376a21255d333915d29f1a652a59c24d8e7
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2023-05-29 13:14:20 +0300

    update places_service urls with separate closes_point

--- places_service/urls.py
+++ places_service/urls.py
@@ -4,11 +4,12 @@ from rest_framework.routers import SimpleRouter
 from places_service.views import PlaceViewSet
 
 router = SimpleRouter()
-router.register("places", PlaceViewSet, basename="places")
+router.register("places", PlaceViewSet, basename="place")
 
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("places/closest_point/", PlaceViewSet.as_view({"get": "closest_point"}), name="place-closest-point"),
 ]
 
 app_name = "places_service"
