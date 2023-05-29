48c809776029bdc76b3ac5cb72b2850ce13b0b86
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2023-05-29 07:36:25 +0300

    add PlaceViewSet with pagination and custom action

--- places_service/views.py
+++ places_service/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 
-# Create your views here.
+from django.contrib.gis.geos import Point
+from django.contrib.gis.db.models.functions import Distance
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from places_service.models import Place
+from places_service.serializers import PlaceSerializer
+
+
+class PlacePagination(PageNumberPagination):
+    page_size = 5
+    max_page_size = 1_000
+
+
+class PlaceViewSet(viewsets.ModelViewSet):
+    queryset = Place.objects.all()
+    serializer_class = PlaceSerializer
+    pagination_class = PlacePagination
+
+    @action(detail=False, methods=["get"])
+    def closest_point(self, request):
+        latitude = float(request.query_params.get("latitude"))
+        longitude = float(request.query_params.get("longitude"))
+
+        point = Point(longitude, latitude, srid=4326)
+
+        closest_place = (
+            Place.objects.annotate(distance=Distance("geom", point))
+            .order_by("distance")
+            .first()
+        )
+
+        serializer = self.get_serializer(closest_place)
+        return Response(serializer.data)
