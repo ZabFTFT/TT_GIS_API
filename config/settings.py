ed8d9a74f45cb01ecb427887874997da86aba099
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2023-05-29 07:34:10 +0300

    update settings with allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = "django-insecure-q5(^fk*7^dr)@&0k+)p5wnsf77&kry^3$960v$(jhh*+$^=55m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "0.0.0.0",
+    "127.0.0.1",
+]
 
 INTERNAL_IPS = [
     "127.0.0.1",
