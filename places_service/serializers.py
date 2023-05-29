e0f1129e5506b90c7f3e9963d321f8648ae7100a
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2023-05-29 10:39:32 +0300

    update geom with GeometryField

--- places_service/serializers.py
+++ places_service/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework_gis.serializers import GeometryField
 
 from places_service.models import Place
 
 
 class PlaceSerializer(ModelSerializer):
+    geom = GeometryField()
+
     class Meta:
         model = Place
         fields = (
